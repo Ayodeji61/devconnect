a9f71c931e2b2f456dbb25d21f866bff9905d677
Ayodeji61 <alabidejimichael@gmail.com>
2020-07-31 18:03:41 +0100

    second push

--- server.js
+++ server.js
@@ -44,6 +44,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
